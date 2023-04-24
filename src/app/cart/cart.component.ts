b93c5dd7c9d3fc8ce112ff8877a2964737ad5e83
vanphuong02 <106482645+vanphuong02@users.noreply.github.com>
2023-04-24 08:21:38 +0700

    cart

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -10,22 +10,21 @@ import { Router } from '@angular/router';
 export class CartComponent implements OnInit {
   product: Product[] = []
   total: number = 0;
-
   constructor(private card: ColorService, private route: Router) {
   }
   ngOnInit(): void {
-    const productCart = JSON.parse(sessionStorage.getItem('localCart') || '{}');
+    const productCart = JSON.parse(sessionStorage.getItem('localCart') || '[]');
     const { id, ShoeName, code, color, gender, img, price, quantity, size } = productCart
     productCart.forEach((element: Product) => {
       this.product.push(element);
       console.log(element);
     })
-    this.getCart()
+    this.getCart();
   }
   removeall() {
     sessionStorage.removeItem('localCart');
     if (sessionStorage.getItem('localCart') === null) {
-      location.reload(); // reload trang
+      location.reload();
     }
   }
   getCart(): void {
@@ -39,5 +38,14 @@ export class CartComponent implements OnInit {
   getTotal(items: Product[]): number {
     return items.reduce((total, item) => total + (item.price * item.quantity), 0);
   }
-  
+  deleteOne(index: number) {
+    const cart = JSON.parse(sessionStorage.getItem('localCart') || '[]') as Product[];
+    if (cart ) {
+      cart.splice(index, 1);
+      sessionStorage.setItem('localCart', JSON.stringify(cart));
+      this.product = cart;
+      this.total = this.getTotal(this.product);
+      location.reload();
+    }
+  }
 }
